4f81a61f72a0b4848602a958665727238797631d
Jason Howie <howiejason93@gmail.com>
2019-03-01 16:40:02 -0500

    Updates

---
+++ precache-manifest.b760aeb60c3330a4bd50f39c0774d77b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/todo-list/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "86494e9c6d32fb67be2b",
+    "url": "/todo-list/static/js/runtime~main.86494e9c.js"
+  },
+  {
+    "revision": "97c949e1cce04c9915b8",
+    "url": "/todo-list/static/js/main.97c949e1.chunk.js"
+  },
+  {
+    "revision": "c32ca5aa67a0dbbed501",
+    "url": "/todo-list/static/js/2.c32ca5aa.chunk.js"
+  },
+  {
+    "revision": "97c949e1cce04c9915b8",
+    "url": "/todo-list/static/css/main.2f208690.chunk.css"
+  },
+  {
+    "revision": "3e2757ff3b2f2e9a7aadcd804a162114",
+    "url": "/todo-list/index.html"
+  }
+];
